bd877c422e21755a521f58c452de35f0525a502f
patrick <patrickchow0803@gmail.com>
2019-12-23 15:40:27 -0500

    Added Timer with Handler

--- andfun-kotlin-dessert-pusher-starter-code/andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ andfun-kotlin-dessert-pusher-starter-code/andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -33,6 +33,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -75,6 +76,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
+
+        dessertTimer = DessertTimer()
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -153,7 +156,16 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
-        Log.i("MainActivity", "onStart Called")
         Timber.i("onStart Called")
+
+        dessertTimer.startTimer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Timber.i("onStop Called")
+
+        dessertTimer.stopTimer()
     }
 }
